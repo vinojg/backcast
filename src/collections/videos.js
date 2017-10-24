0b28c221f74491520d5ffe099e12f04b41de3aa2
Hack Reactor Student <student@hackreactor.com>
2017-10-24 15:49:42 -0700

    Created a reusable API helper

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,25 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  
+  parse: function(response) {
+    return response.items;
+  },
+  
+  search: function(string) {
+    this.fetch({
+      data: {
+        part: 'snippet',
+        q: string,
+        maxResults: '5',
+        key: YOUTUBE_API_KEY,
+        type: 'video',
+        videoEmbeddable: 'true'
+      }
+    });
+  }
+
 
 });
